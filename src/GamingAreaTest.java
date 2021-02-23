99b34946163fca6fa94e627a7773bd714379c33c
samsara <wk1044481101@163.com>
2021-02-23 15:07:16 +0800

    A

--- src/GamingAreaTest.java
+++ src/GamingAreaTest.java
@@ -10,13 +10,8 @@ public class GamingAreaTest {
 
 
     GamingArea gamingArea = new GamingArea(5, 10);
-//    boolean[][] testBoard = {
-//            {true, false, true, false, false, false, false, false, false, false},
-//            {true, true, false, false, false, false, false, false, false, false},
-//            {true, false, true, false, false, false, false, false, false, false},
-//            {true, true, true, false, false, false, false, false, false, false},
-//            {true, true, true, false, false, false, false, false, false, false}
-//    };
+    Shape t;
+    Shape t2;
 
     @Rule
     public Timeout timeout = new Timeout(100);
@@ -26,8 +21,13 @@ public class GamingAreaTest {
     public void before() {
 
         assertTrue("true", gamingArea.committed);
-        Shape testBoard = new Shape("0 0  1 0  2 0  3 0  4 0  1 1  2 2  0 2  3 1  3 2  4 1  4 2");
+        Shape testBoard = new Shape("0 1  1 0  3 0  4 0  2 1  3 1  4 1");
         gamingArea.place(testBoard, 0, 0);
+        gamingArea.commit();
+
+        t = new Shape("0 1  1 0  1 1  2 1");
+        t2 = new Shape("0 1  1 0  1 1  1 2");
+
     }
 
 
@@ -36,8 +36,6 @@ public class GamingAreaTest {
 
         assertEquals(5, gamingArea.getAreaWidth());
         assertEquals(10, gamingArea.getAreaHeight());
-        assertFalse(gamingArea.committed);
-        gamingArea.commit();
         assertTrue(gamingArea.committed);
     }
 
@@ -45,27 +43,31 @@ public class GamingAreaTest {
     @Test
     public void getMaxHeightTest() {
 
+        //空场景高度为0
         GamingArea gamingArea2 = new GamingArea(5, 10);
         assertEquals(0, gamingArea2.getMaxHeight());
 
+        //一个点高度为1
         gamingArea2.commit();
         Shape testBoard = new Shape("0 0 ");
         gamingArea2.place(testBoard, 0, 0);
         assertEquals(1, gamingArea2.getMaxHeight());
 
 
-        Shape t11 = new Shape("0 1  1 0  1 1  2 1");
-        gamingArea.commit();
-        assertEquals(0, gamingArea.place(t11, 0, 3));
-        assertEquals(5, gamingArea.getMaxHeight());
+        gamingArea.place(t, 2, 2);
+        assertEquals(4, gamingArea.getMaxHeight());
         gamingArea.undo();
 
-        assertEquals(0, gamingArea.place(t11, 0, 9));
+        gamingArea.place(t, 0, 8);
         assertEquals(10, gamingArea.getMaxHeight());
         gamingArea.undo();
 
-        assertEquals(0, gamingArea.place(t11, 0, 8));
+        gamingArea.place(t, 0, 9);
         assertEquals(10, gamingArea.getMaxHeight());
+        gamingArea.undo();
+
+        gamingArea.place(t, 0, 10);
+        assertEquals(2, gamingArea.getMaxHeight());
         gamingArea.undo();
     }
 
@@ -73,7 +75,6 @@ public class GamingAreaTest {
     @Test
     public void getFilledBlockCountTest() {
 
-        System.out.println(gamingArea);
         assertEquals(0, gamingArea.getFilledBlockCount(-1));
         assertEquals(3, gamingArea.getFilledBlockCount(0));
         assertEquals(4, gamingArea.getFilledBlockCount(1));
@@ -128,79 +129,129 @@ public class GamingAreaTest {
     @Test
     public void placeTest() {
 
-        Shape t = new Shape("0 1  1 0  1 1  2 1");
-        Shape t2 = new Shape("0 1  1 0  1 1  1 2");
         Shape l = new Shape("0 0  0 1  0 2  0 3");
-        gamingArea.commit();
+
+        Shape testBoard = new Shape("0 1  1 0  3 0  4 0  2 1  3 1  4 1  2 3  3 3  3 2  4 3");
+        GamingArea gamingArea1 = new GamingArea(5, 10);
+        gamingArea1.place(testBoard, 0, 0);
+        gamingArea.place(t,2,2);
+        areaEquals(gamingArea1,gamingArea);
+        gamingArea.undo();
+
+        assertEquals(0, gamingArea.place(t, 2, 2));
+        gamingArea.undo();
+        assertEquals(0, gamingArea.place(t, 0, 8));
+        gamingArea.undo();
+        assertEquals(0, gamingArea.place(t, 0, 9));
+        gamingArea.undo();
+        assertEquals(0, gamingArea.place(t, 0, 10));
+        gamingArea.undo();
+
+
         assertEquals(1, gamingArea.place(t, 0, 1));
-        System.out.println(gamingArea);
         assertFalse(gamingArea.committed);
         try {
-            gamingArea.committed = false;
+            gamingArea.place(t, 4, 0);
         } catch (Exception e) {
             assertEquals(e.getMessage(), "未commit时调用place");
         }
-        gamingArea.commit();
-        assertTrue(gamingArea.committed);
+        gamingArea.undo();
+
         assertEquals(2, gamingArea.place(t, 4, 0));
         gamingArea.undo();
         assertEquals(2, gamingArea.place(t, -1, 0));
         gamingArea.undo();
         assertEquals(2, gamingArea.place(t, 0, -1));
         gamingArea.undo();
-        assertEquals(0, gamingArea.place(t, 0, 9));
-        gamingArea.commit();
+        assertEquals(2, gamingArea.place(l, -3, 0));
+        gamingArea.undo();
+
         assertEquals(3, gamingArea.place(t, 0, 0));
         gamingArea.undo();
-        assertEquals(0, gamingArea.place(t, 1, 2));
-
+        assertEquals(3, gamingArea.place(t, 1, 0));
+        gamingArea.undo();
+        assertEquals(3, gamingArea.place(t, 2, 1));
+        gamingArea.undo();
+        assertEquals(3, gamingArea.place(t, 2, 0));
+        gamingArea.undo();
+        assertEquals(3, gamingArea.place(l, 0, 0));
+        gamingArea.undo();
+        assertEquals(3, gamingArea.place(l, 0, 1));
         gamingArea.undo();
 
-        assertEquals(0, gamingArea.place(t2, 3, 1));
+    }
 
+    @Test
+    public void  commitTest(){
+        gamingArea.place(t,0,1);
+        assertFalse(gamingArea.committed);
         gamingArea.commit();
-        assertEquals(0, gamingArea.place(t, 2, 9));
-
-        gamingArea.commit();
-        assertEquals(0, gamingArea.place(l, 4, 7));
+        assertTrue(gamingArea.committed);
     }
 
     @Test
     public void  undoTest(){
 
-        GamingArea gamingArea1 = new GamingArea(5, 10);
-        assertEquals(0, gamingArea1.getMaxHeight());
+        GamingArea gamingArea1 = new GamingArea(5,10);
+        Shape testBoard = new Shape("0 0  1 0  2 0  3 0  4 0  1 1  2 2  0 2  3 1  3 2  4 1  4 2");
+        gamingArea1.place(testBoard, 0, 0);
+        gamingArea1.commit();
 
-        GamingArea gamingArea2 = new GamingArea(5, 10);
-        assertEquals(0, gamingArea2.getMaxHeight());
+        gamingArea.place(t,2,2);
 
-        gamingArea2.commit();
-        Shape testBoard = new Shape("0 0 ");
-        gamingArea2.place(testBoard, 0, 0);
-        gamingArea2.undo();
-        for (int x = 0; x < gamingArea2.getAreaWidth(); x++) {
-            for (int y = 0; y < gamingArea2.getAreaHeight(); y++) {
-                assertEquals(gamingArea1.isFilled(x,y),gamingArea2.isFilled(x,y));
+        assertFalse(gamingArea.committed);
+        gamingArea.undo();
+        assertTrue(gamingArea.committed);
+
+        areaEquals(gamingArea1, gamingArea);
+        gamingArea.undo();
+        areaEquals(gamingArea1, gamingArea);
+    }
+
+
+    private void areaEquals(GamingArea gamingArea, GamingArea gamingArea1) {
+
+        for (int x = 0; x < gamingArea.getAreaWidth(); x++) {
+            for (int y = 0; y < gamingArea.getAreaHeight(); y++) {
+                assertEquals(gamingArea.isFilled(x, y), gamingArea1.isFilled(x, y));
             }
         }
     }
 
+
     @Test
     public void clearTest() {
 
-        Shape testBoard = new Shape("0 0  1 0  2 0  3 0  4 0  1 1  2 2  0 2");
+        Shape l = new Shape("0 0  0 1  0 2  0 3");
+        Shape l2 = new Shape("0 0  2 0  3 0  4 0");
+        Shape t2 = new Shape("0 1  1 0  1 1  2 1  3 1  4 1");
+
+        Shape testBoard = new Shape("1 0  3 0  4 0  0 1  1 1  2 1");
         GamingArea gamingArea1 = new GamingArea(5, 10);
         gamingArea1.place(testBoard, 0, 0);
+        gamingArea.place(t,0,1);
+        areaEquals(gamingArea, gamingArea1);
+        gamingArea.undo();
 
-        Shape testBoard2 = new Shape("1 0  2 1  0 1");
+        Shape testBoard2 = new Shape("1 0  3 0  4 0");
         GamingArea gamingArea2 = new GamingArea(5, 10);
         gamingArea2.place(testBoard2, 0, 0);
+        gamingArea.place(t2,0,1);
+        areaEquals(gamingArea, gamingArea2);
+        gamingArea.undo();
 
-        for (int x = 0; x < gamingArea2.getAreaWidth(); x++) {
-            for (int y = 0; y < gamingArea2.getAreaHeight(); y++) {
-                assertEquals(gamingArea1.isFilled(x,y),gamingArea2.isFilled(x,y));
-            }
-        }
 
+
+        Shape testBoard3 = new Shape("1 0  1 1  3 0  4 0");
+        GamingArea gamingArea3 = new GamingArea(5, 10);
+        gamingArea3.place(testBoard3, 0, 0);
+        gamingArea.place(l2, 0, 2);
+        gamingArea.commit();
+        gamingArea.place(l2, 0, 3);
+        gamingArea.commit();
+        gamingArea.place(l2, 0, 3);
+        gamingArea.commit();
+        gamingArea.place(l, 1,1);   //多行消除
+        areaEquals(gamingArea,gamingArea3);
     }
 }
