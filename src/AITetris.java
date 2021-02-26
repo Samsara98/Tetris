feac826c7396f3ffb90633f814d6752282ffaae8
samsara <wk1044481101@163.com>
2021-02-26 14:04:30 +0800

    A

--- src/AITetris.java
+++ src/AITetris.java
@@ -8,6 +8,7 @@ public class AITetris extends Tetris implements AI {
 
     JCheckBox debugButton = new JCheckBox();
 
+
     AITetris(int pixels) {
 
         super(pixels);
@@ -22,8 +23,13 @@ public class AITetris extends Tetris implements AI {
             gamingArea.undo();
             Move move = calculateBestMove(gamingArea, currentShape);
             if (null != move) {
-                currentX = move.x;
-                currentY = move.y;
+                if (move.x > newX){
+                    super.tick(RIGHT);
+                }else if (move.x < newX){
+                    super.tick(LEFT);
+                }else {
+                    super.tick(direction);
+                }
                 currentShape = move.shape;
                 score = (int) move.score;
             }
@@ -35,77 +41,15 @@ public class AITetris extends Tetris implements AI {
     @Override
     public JPanel createControlPanel() {
 
-        JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-
-        countLabel = new JLabel("0");
-        panel.add(countLabel);
-
-        scoreLabel = new JLabel("0");
-        panel.add(scoreLabel);
-
-        timeLabel = new JLabel(" ");
-        panel.add(timeLabel);
-
-        panel.add(Box.createVerticalStrut(20));             // 空白间隔
-
-        startButton = new JButton("开始");
-        startButton.addActionListener(e -> startGame());
-        panel.add(startButton);
-
-        stopButton = new JButton("暂停");
-        stopButton.addActionListener(e -> stopGame());
-        panel.add(stopButton);
-        toggleButtons();
-
-        panel.add(Box.createVerticalStrut(20));
-
-        speedSlider = new JSlider(0, 200, 100);
-        speedSlider.setPreferredSize(new Dimension(100, 15));
-        speedSlider.addChangeListener(e -> updateTimer());
-        updateTimer();
-        panel.add(speedSlider);
-
-        panel.add(Box.createVerticalStrut(12));
-
-        quitButton = new JButton("退出");
-        quitButton.addActionListener(e -> System.exit(0));
-        panel.add(quitButton);
-
+        JPanel panel = super.createControlPanel();
         random = new Random(); // 用于让形状随机出现
         debugButton = new JCheckBox("debug");
-        ActionListener actionListener = new ActionListener(){
-            public void actionPerformed(ActionEvent e) {
-                if(debugButton.isSelected()) {
-                    random.setSeed(0);
-                }else {
-                    random.setSeed(System.currentTimeMillis());
-                }
-            }
-        };
-        debugButton.addActionListener(actionListener);
+        debugButton.addActionListener(debugButton.isSelected()?e -> random.setSeed(0):e -> random.setSeed(System.currentTimeMillis()));
         panel.add(debugButton);
 
         return panel;
     }
 
-    @Override
-    public void startGame() {
-
-        count = 0;
-        score = 0;
-        gameOn = true;
-        startTime = System.currentTimeMillis();
-        timer.start();
-
-        gamingArea = new GamingArea(WIDTH, HEIGHT + TOP_SPACE);
-        updateCounters();
-        toggleButtons();
-        timeLabel.setText(" ");
-        addNewShape();
-
-        repaint();
-    }
 
     public static void main(String[] args) {
         // boilerplate code
